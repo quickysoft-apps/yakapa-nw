79b8dc00987d2b7e5555ffde26ca3c57eab2ef83
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-02-25 17:35:15 +0100

    POC: display app

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -2,25 +2,45 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Button from '@material-ui/core/Button';
 
-class App extends Component {
+interface State {
+  source?: string;
+}
+
+class App extends Component<{}, State> {
+  /**
+   *
+   */
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      source: undefined
+    };
+  }
   render() {
     return (
       <div>
         <Button onClick={this.onClick} variant="contained" color="primary">
           Hello World
         </Button>
+        <webview src={this.state.source} />
       </div>
     );
   }
 
   private readonly onClick = () => {
-    chrome.tabs.create({ url: 'chrome-extension://kffcccokkgaooalajeihldalfjkfokob/dist/index.html' });
-    chrome.management.getAll(result => {
-      console.log(result);
+    this.setState({
+      source:
+        'chrome-extension://kffcccokkgaooalajeihldalfjkfokob/dist/index.html'
     });
-    chrome.runtime.sendMessage({ injectApp: true }, response => {
-      console.log('RESPONSE', response);
-    });
+    // chrome.tabs.create({
+    //   url: 'chrome-extension://kffcccokkgaooalajeihldalfjkfokob/dist/index.html'
+    // });
+    // chrome.management.getAll(result => {
+    //   console.log(result);
+    // });
+    // chrome.runtime.sendMessage({ injectApp: true }, response => {
+    //   console.log('RESPONSE', response);
+    // });
     // chrome.tabs.query({ index: 0 }, tabs => {
     //   const tabId = tabs[0].id || tabs[0].index;
     //   chrome.tabs.sendMessage(tabId, { injectApp: true });
