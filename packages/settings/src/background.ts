109d47eef0aa6e695cd9d3977eec587ced31eb1b
neurosoup <neurosoup@gmail.com>
2019-02-26 21:49:20 +0100

    POC: jobs

--- packages/settings/src/background.ts
+++ packages/settings/src/background.ts
@@ -1,12 +1,11 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  alert(JSON.stringify(chrome.notifications));
-  alert(JSON.stringify(chrome.alarms));
-  // chrome.alarms.create("job", {
-  //     delayInMinutes: 0.1,
-  //     periodInMinutes: 0.1
-  //   });
+  chrome.alarms.create("job", {
+    delayInMinutes: 0.1,
+    periodInMinutes: 0.1
+  });
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   console.log("Got an alarm!", alarm);
+  alert("ALEEEEERTE");
 });
