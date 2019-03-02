8b921448443c7d01753f7c97de4fadc1770f0f6b
neurosoup <neurosoup@gmail.com>
2019-03-02 10:03:13 +0100

    POC: packaging with extensions

--- packages/settings/src/content.tsx
+++ packages/settings/src/content.tsx
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import { render } from "react-dom";
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import { render } from 'react-dom';
 
 class Content extends Component {
   render() {
     return (
       <div>
         <Button onClick={this.onClick} variant="contained" color="primary">
-          Je suis une fucking EXTENSION !
+          Je suis une fucking EXTENSION !ddfggfgdd
         </Button>
       </div>
     );
   }
 
   private readonly onClick = () => {
-    chrome.runtime.sendMessage("background");
+    chrome.runtime.sendMessage('background');
   };
 }
 
-document.addEventListener("injectExtension", data => {
+document.addEventListener('injectExtension', data => {
   injectExtension();
 });
 
 function injectExtension() {
-  const root = document.getElementById("extension");
+  const root = document.getElementById('extension');
   if (root) {
     render(<Content />, root);
   }
