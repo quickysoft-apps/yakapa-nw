e597ebe04458c07387c60a4644fc7e972b2fc9ed
neurosoup <neurosoup@gmail.com>
2019-03-07 00:12:40 +0100

    add pretty ui in shell

--- packages/shell/src/App.tsx
+++ packages/shell/src/App.tsx
@@ -1,15 +1,155 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Button from '@material-ui/core/Button';
+import {
+  withStyles,
+  AppBar,
+  CssBaseline,
+  Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  ListItem
+} from '@material-ui/core';
+import MailIcon from '@material-ui/icons/Mail';
+import InboxIcon from '@material-ui/icons/Inbox';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class App extends Component {
+const drawerWidth = 240;
+
+const styles = (theme: any) => ({
+  root: {
+    display: 'flex'
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  }
+});
+
+interface Props {
+  classes: any;
+  theme: any;
+  container?: any;
+}
+
+interface State {
+  mobileOpen: boolean;
+}
+
+class ResponsiveDrawer extends React.Component<Props, State> {
+  /**
+   *
+   */
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      mobileOpen: false
+    };
+  }
+
+  handleDrawerToggle = () => {
+    this.setState((state: any) => ({ mobileOpen: !state.mobileOpen }));
+  };
+
   render() {
-    return (
+    const { classes, theme } = this.props;
+
+    const drawer = (
       <div>
-        <Button onClick={this.onClick} variant="contained" color="primary">
-          Hello World !
-        </Button>
-        <div id="extension" />
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text} onClick={this.onClick}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Open drawer" onClick={this.handleDrawerToggle} className={classes.menuButton}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <nav className={classes.drawer}>
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={this.props.container}
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper
+              }}
+              variant="permanent"
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <div id="extension" />
+        </main>
       </div>
     );
   }
@@ -23,18 +163,20 @@ class App extends Component {
         chrome.management.getPermissionWarningsById(ext.id, warnings => {
           warnings.forEach(x => console.log(x));
         });
-        injectExtension(ext.id);
+        this.injectExtension(ext.id);
       }
     });
   };
+
+  injectExtension = (id: string) => {
+    const chromeExtensionUrl = `chrome-extension://${id}`;
+    console.log('Inject', `(${chromeExtensionUrl})`);
+    const event = document.createEvent('Event');
+    event.initEvent(JSON.stringify({ inject: id }));
+    document.dispatchEvent(event);
+  };
 }
 
-const injectExtension = (id: string) => {
-  const chromeExtensionUrl = `chrome-extension://${id}`;
-  console.log('Inject', `(${chromeExtensionUrl})`);
-  const event = document.createEvent('Event');
-  event.initEvent(JSON.stringify({ inject: id }));
-  document.dispatchEvent(event);
-};
+const App = withStyles(styles, { withTheme: true })(ResponsiveDrawer);
 
 render(<App />, document.getElementById('root'));
