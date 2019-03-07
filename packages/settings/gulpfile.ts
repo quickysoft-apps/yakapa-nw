f46538b8c7969a388e78ada09f10374add625f8d
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-03-07 18:08:21 +0100

    POC: start nw with logs

--- packages/settings/gulpfile.ts
+++ packages/settings/gulpfile.ts
@@ -1,30 +1,31 @@
-import { series, src, dest, watch } from 'gulp';
-import del from 'del';
-import ParcelBundler from 'parcel-bundler';
+import { series, src, dest, watch } from "gulp";
+import del from "del";
+import ParcelBundler from "parcel-bundler";
 
 type TaskCallback = (err?: Error) => void;
 
 function clean(cb: TaskCallback) {
-  return del(['./dist/**/*.+(js|html|map|json)']);
+  return del(["./dist/**/*.+(js|html|map|json)"]);
 }
 
 function copyManifest(cb: TaskCallback) {
-  return src('manifest.json').pipe(dest('./dist/'));
+  return src("manifest.json").pipe(dest("./dist/"));
 }
 
 async function startParcel(cb: TaskCallback) {
-  const bundler = new ParcelBundler('src/**/*', {
+  const bundler = new ParcelBundler("src/**/*", {
     watch: true,
-    outDir: './dist/build'
+    publicUrl: "./",
+    outDir: "./dist/build"
   });
   await bundler.bundle();
   cb();
 }
 
 async function buildParcel(cb: TaskCallback) {
-  const bundler = new ParcelBundler('src/**/*', {
+  const bundler = new ParcelBundler("src/**/*", {
     watch: false,
-    outDir: './dist/build'
+    outDir: "./dist/build"
   });
   await bundler.bundle();
   cb();
@@ -34,4 +35,4 @@ async function buildParcel(cb: TaskCallback) {
 export const start = series(clean, copyManifest, startParcel);
 export const build = series(clean, copyManifest, buildParcel);
 
-watch('manifest.json', { queue: true }, copyManifest);
+watch("manifest.json", { queue: true }, copyManifest);
--- packages/shell/gulpfile.ts
+++ packages/shell/gulpfile.ts
@@ -1,42 +1,62 @@
-import { series, src, dest } from 'gulp';
-import del from 'del';
-import ParcelBundler from 'parcel-bundler';
-import { Runner, Builder } from 'nwjs-builder-phoenix';
+import { series, src, dest } from "gulp";
+import del from "del";
+import ParcelBundler from "parcel-bundler";
+import { Builder } from "nwjs-builder-phoenix";
+import { spawn } from "child_process";
 
 type TaskCallback = (err?: Error) => void;
 
-const buildDestination = '../../release';
-const extensionsDist = './dist/extensions/';
+const buildDestination = "../../release";
+const extensionsDist = "./dist/extensions/";
 
 function cleanStart() {
-  return del(['dist/*.+(js|html|map)']);
+  return del(["dist/*.+(js|html|map)"]);
 }
 
 async function startParcel(cb: TaskCallback) {
-  const bundler = new ParcelBundler('src/**/*', {
+  const bundler = new ParcelBundler("src/**/*", {
     watch: true,
-    publicUrl: './'
+    publicUrl: "./"
   });
   await bundler.bundle();
   cb();
 }
 
 async function startNW(cb: TaskCallback) {
-  const runner = new Runner({ x64: true, mirror: 'https://dl.nwjs.io/', detached: false }, ['.']);
-  runner.run().then(code => {
-    process.exit(code);
+  // const runner = new Runner(
+  //   { x64: true, mirror: "https://dl.nwjs.io/", detached: false },
+  //   ["."]
+  // );
+  // runner.run().then(code => {
+  //   process.exit(code);
+  // });
+
+  const process = spawn("./node_modules/.bin/run", [
+    "--x64",
+    "--mirror https://dl.nwjs.io/",
+    "."
+  ]);
+  process.stdout.setEncoding("utf8");
+  process.stdout.on("data", function(data) {
+    const str = data.toString();
+    const lines = str.split(/(\r?\n)/g);
+    console.log(lines.join(""));
+  });
+
+  process.on("close", function(code) {
+    console.log("process exit code " + code);
   });
   cb();
 }
 
 function createReleaseFolder() {
-  return src('*.*', { read: false }).pipe(dest(buildDestination));
+  return src("*.*", { read: false }).pipe(dest(buildDestination));
 }
 
 function copyExtensions() {
-  return src('*.*', { read: false })
+  return src("*.*", { read: false })
     .pipe(dest(extensionsDist))
-    .pipe(src('../settings/dist/**/*'))
+    .pipe(src("../settings/dist/**/*"))
     .pipe(dest(`${extensionsDist}/settings/`));
 }
 
@@ -46,19 +66,33 @@ async function cleanBuild() {
 }
 
 async function buildParcel(cb: TaskCallback) {
-  const bundler = new ParcelBundler('src/**/*', {
+  const bundler = new ParcelBundler("src/**/*", {
     watch: false,
-    publicUrl: './'
+    publicUrl: "./"
   });
   await bundler.bundle();
   cb();
 }
 
 async function buildNW(cb: TaskCallback) {
-  const builder = new Builder({ win: true, x64: true, mirror: 'https://dl.nwjs.io/', destination: buildDestination }, '.');
+  const builder = new Builder(
+    {
+      win: true,
+      x64: true,
+      mirror: "https://dl.nwjs.io/",
+      destination: buildDestination
+    },
+    "."
+  );
   await builder.build();
   cb();
 }
 
 export const start = series(cleanStart, startParcel, startNW);
-export const build = series(createReleaseFolder, cleanBuild, copyExtensions, buildParcel, buildNW);
+export const build = series(
+  createReleaseFolder,
+  cleanBuild,
+  copyExtensions,
+  buildParcel,
+  buildNW
+);
