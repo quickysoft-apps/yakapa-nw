595b5bef3a6dd812e0ad5b68bf5823985df9af26
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-03-14 13:46:13 +0100

    feature: use common npmRun in runner package gulpfile

--- packages/runner/gulpfile.ts
+++ packages/runner/gulpfile.ts
@@ -1,62 +1,33 @@
-import { series, src, dest, watch } from "gulp";
-import del from "del";
-import ParcelBundler from "parcel-bundler";
-import path from "path";
-import { ChildProcess, spawn } from "child_process";
+import { series, src, dest, watch } from 'gulp';
+import del from 'del';
+import ParcelBundler from 'parcel-bundler';
+import { npmRun } from '@yakapa/common';
 
 type TaskCallback = (err?: Error) => void;
 
-function onExit(childProcess: ChildProcess): Promise<void> {
-  return new Promise((resolve, reject) => {
-    childProcess.once("exit", (code: number, signal: string) => {
-      if (code === 0) {
-        resolve(undefined);
-      } else {
-        reject(new Error("Exit with error code: " + code));
-      }
-    });
-    childProcess.once("error", (err: Error) => {
-      reject(err);
-    });
-  });
-}
-
-async function npmRun(command: string, args: string[]) {
-  const fullpath = path.resolve(__dirname, command);
-  const npmProcess = spawn(fullpath, args, {
-    stdio: [process.stdin, process.stdout, process.stderr]
-  });
-
-  npmProcess.on("close", function(code) {
-    process.exit(code);
-  });
-
-  onExit(npmProcess);
-}
-
 function clean() {
-  return del(["./dist/**/*.+(js|html|map|json)"]);
+  return del(['./dist/**/*.+(js|html|map|json)']);
 }
 
 function copyManifest() {
-  return src("manifest.json").pipe(dest("./dist/"));
+  return src('manifest.json').pipe(dest('./dist/'));
 }
 
 async function startParcel(cb: TaskCallback) {
-  npmRun("./node_modules/.bin/parcel", [
-    "watch",
-    "src/**/*",
-    "--public-url",
-    " ./",
-    "--out-dir",
-    "./dist/build"
+  npmRun('parcel', [
+    'watch',
+    'src/**/*',
+    '--public-url',
+    ' ./',
+    '--out-dir',
+    './dist/build'
   ]).then(_ => cb());
 }
 
 async function buildParcel(cb: TaskCallback) {
-  const bundler = new ParcelBundler("src/**/*", {
+  const bundler = new ParcelBundler('src/**/*', {
     watch: false,
-    outDir: "./dist/build"
+    outDir: './dist/build'
   });
   await bundler.bundle();
   cb();
@@ -66,4 +37,4 @@ async function buildParcel(cb: TaskCallback) {
 export const start = series(clean, copyManifest, startParcel);
 export const build = series(clean, copyManifest, buildParcel);
 
-watch("manifest.json", { queue: true }, copyManifest);
+watch('manifest.json', { queue: true }, copyManifest);
