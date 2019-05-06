18f40eef7fc8433488bb4512f6892f3c80ad68b9
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-05-06 21:52:30 +0200

    agent settings with formik

--- packages/settings/src/pages/AgentSettings.tsx
+++ packages/settings/src/pages/AgentSettings.tsx
@@ -1,135 +1,27 @@
-import React, { useReducer, ChangeEvent, FormEvent, Dispatch } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { prisma } from '@yakapa/api';
+import React, { FC } from 'react';
+import { Formik, FormikActions, FormikProps, Form, Field, FieldProps } from 'formik';
+import { TextField } from 'formik-material-ui';
 
-interface State {
+interface MyFormValues {
   nickname: string;
-  nicknameError: string;
-  emailAddress: string;
-  emailAddressError: string;
-  submitted: boolean;
 }
 
-const defaultState = {
-  nickname: '',
-  nicknameError: '',
-  emailAddress: '',
-  emailAddressError: '',
-  submitted: false
-};
-
-interface SetNickname {
-  type: 'SET_NICKNAME';
-  value: string;
-}
-
-interface SetEmailAddress {
-  type: 'SET_EMAIL_ADDRESS';
-  value: string;
-}
-
-interface Submit {
-  type: 'SUBMIT';
-  nickname: string;
-  emailAddress: string;
-}
-
-type Actions = SetNickname | SetEmailAddress | Submit;
-
-const validateNickname = (value: string) => {
-  const error = value ? '' : 'You must enter a nickname';
-  return error;
-};
-
-const validateEmailAddress = (value: string): string => {
-  const error = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
-    ? ''
-    : 'You must enter a valid email address';
-  return error;
-};
-
-function dispatchMiddleware(dispatch: Dispatch<Actions>) {
-  return async (action: Actions) => {
-    if (action.type === 'SUBMIT') {
-      const agent = await prisma.createAgent({
-        nickname: action.nickname,
-        email: action.emailAddress
-      });
-      console.log('Agent created', agent);
-    }
-    dispatch(action);
-  };
-}
-
-function reducer(state: State, action: Actions) {
-  switch (action.type) {
-    case 'SET_NICKNAME':
-      return {
-        ...state,
-        nickname: action.value,
-        nicknameError: validateNickname(action.value)
-      };
-    case 'SET_EMAIL_ADDRESS':
-      return {
-        ...state,
-        emailAddress: action.value,
-        emailAddressError: validateEmailAddress(action.value)
-      };
-    case 'SUBMIT':
-      const nicknameError = validateNickname(action.nickname);
-      const emailAddressError = validateEmailAddress(action.emailAddress);
-      if (nicknameError === '' && emailAddressError === '') {
-        return {
-          ...state,
-          submitted: true
-        };
-      } else {
-        return {
-          ...state,
-          nicknameError,
-          emailAddressError
-        };
-      }
-    default:
-      return state;
-  }
-}
-
-export const AgentSettings = () => {
-  const [{ submitted, ...state }, dispatch] = useReducer(reducer, defaultState);
-
+export const AgentSettings: FC<{}> = () => {
   return (
-    <form
-      noValidate
-      autoComplete="off"
-      onSubmit={(e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const dispatchWithEffect = dispatchMiddleware(dispatch);
-        dispatchWithEffect({
-          type: 'SUBMIT',
-          nickname: state.nickname,
-          emailAddress: state.emailAddress
-        });
-      }}
-    >
-      <TextField
-        id="nickname"
-        error={state.nicknameError !== ''}
-        label={state.nicknameError || 'Nickname'}
-        value={state.nickname}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_NICKNAME', value: e.currentTarget.value })}
-        margin="normal"
+    <div>
+      <Formik
+        initialValues={{ nickname: '' }}
+        onSubmit={(values: MyFormValues, actions: FormikActions<MyFormValues>) => {
+          console.log({ values, actions });
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+        render={(formikBag: FormikProps<MyFormValues>) => (
+          <Form>
+            <Field name="nickname" component={TextField} label="Nom de l'agent" margin="normal" fullWidth variant="filled" autoComplete="off" />
+          </Form>
+        )}
       />
-      <TextField
-        id="emailAddress"
-        error={state.emailAddressError !== ''}
-        label={state.emailAddressError || 'Email Address'}
-        value={state.emailAddress}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_EMAIL_ADDRESS', value: e.currentTarget.value })}
-        margin="normal"
-      />
-      <Button type="submit">Save</Button>
-    </form>
+    </div>
   );
 };
