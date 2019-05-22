d2ed9a690fd560049af1b44345f5c5536242c87c
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-05-22 16:47:42 +0200

    add network dialog increment

--- packages/networks/src/components/AddNetworkDialog.tsx
+++ packages/networks/src/components/AddNetworkDialog.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, Button } from '@material-ui/core';
 import { Formik, FormikActions, FormikProps, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
 import { useExtensionEvent } from '@yakapa/shared';
 import { Events } from '../events';
 
@@ -20,29 +21,30 @@ export const AddNetworkDialog: FC<Props> = props => {
 
   const validate = (values: FormValues) => {
     let errors: Errors = {};
+
+    //TODO: async validate uniqueness of network name ?
+    //TODO: validate characters (regex)
+
     return errors;
   };
 
   const submit = async (values: FormValues, actions: FormikActions<FormValues>) => {
     try {
+      //TODO: call api for network creation
       console.log(values);
     } finally {
       actions.setSubmitting(false);
+      setOpen(false);
     }
   };
 
   const cancel = (formikBag: FormikProps<FormValues>) => {
     formikBag.resetForm();
-  };
-
-  const handleClose = () => {
     setOpen(false);
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Ajouter un réseau</DialogTitle>
-
+    <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="form-dialog-title">
       <Formik
         enableReinitialize
         initialValues={{ networkName: '' }}
@@ -50,19 +52,22 @@ export const AddNetworkDialog: FC<Props> = props => {
         onSubmit={submit}
         render={(formikBag: FormikProps<FormValues>) => {
           return (
-            <>
+            <Form>
+              <DialogTitle id="form-dialog-title">Ajouter un réseau</DialogTitle>
               <DialogContent>
-                <Form>
-                  <Field name="networkName" component={TextField} autoFocus label="Nom du réseau" margin="normal" fullWidth autoComplete="off" />
-                </Form>
+                <DialogContentText>
+                  Donnez un nom à votre réseau. Vous pourrez préciser par la suite sa localisation, ses contacts ainsi que plein d'autres détails. Vous pourrez ensuite inviter des
+                  agents dans votre réseau.
+                </DialogContentText>
+                <Field name="networkName" component={TextField} autoFocus label="Nom du réseau" margin="normal" fullWidth autoComplete="off" />
               </DialogContent>
               <DialogActions>
-                <Button onClick={handleClose}>Annuler</Button>
-                <Button onClick={handleClose} color="primary">
+                <Button onClick={() => cancel(formikBag)}>Annuler</Button>
+                <Button disabled={!formikBag.isValid} type="submit" color="primary">
                   Créer
                 </Button>
               </DialogActions>
-            </>
+            </Form>
           );
         }}
       />
