89e29b5d84c8b2d52e956a35433b5ccefeb857a5
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-05-24 23:16:47 +0200

    put code back

--- packages/shared/src/hooks/useLocalStorage.ts
+++ packages/shared/src/hooks/useLocalStorage.ts
@@ -3,24 +3,24 @@ import { useEffect, useState, Dispatch } from 'react';
 export const useLocalStorage = (key: string): [string | undefined, Dispatch<string>] => {
   const [state, setState] = useState<string | undefined>(undefined);
 
-  // useEffect(() => {
-  //   chrome.storage.sync.get(key, result => {
-  //     console.log(`Get from storage ${key}=${result[key]}`);
-  //     setState(result[key]);
-  //   });
-  // }, []);
+  useEffect(() => {
+    chrome.storage.sync.get(key, result => {
+      console.log(`Get from storage ${key}=${result[key]}`);
+      setState(result[key]);
+    });
+  }, []);
 
-  // useEffect(() => {
-  //   chrome.storage.onChanged.addListener(onLocalStorageChange);
-  //   return () => chrome.storage.onChanged.removeListener(onLocalStorageChange);
-  // }, []);
+  useEffect(() => {
+    chrome.storage.onChanged.addListener(onLocalStorageChange);
+    return () => chrome.storage.onChanged.removeListener(onLocalStorageChange);
+  }, []);
 
-  // const onLocalStorageChange = (changes: any) => {
-  //   const change = changes[key];
-  //   if (change) {
-  //     setState(changes[key].newValue);
-  //   }
-  // };
+  const onLocalStorageChange = (changes: any) => {
+    const change = changes[key];
+    if (change) {
+      setState(changes[key].newValue);
+    }
+  };
 
   return [state, (value: any) => chrome.storage.sync.set({ [key]: value })];
 };
